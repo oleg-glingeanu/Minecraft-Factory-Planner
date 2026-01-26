317fc8b37c85feed57a5dc64d2644613dac10d93
SergiuGlingeanu <s.glingeanu@gmail.com>
2026-01-26 13:31:22 +0000

    Trying to add waystone dependancy

--- src/main/java/github/olegglingeanu/factoryplanner/FactoryPlanner.java
+++ src/main/java/github/olegglingeanu/factoryplanner/FactoryPlanner.java
@@ -1,19 +1,57 @@
 package github.olegglingeanu.factoryplanner;
 
 import github.olegglingeanu.factoryplanner.init.ItemInit;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraftforge.client.event.RecipesUpdatedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.server.ServerStartedEvent;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(FactoryPlanner.MODID)
 public class FactoryPlanner {
 
     public static final String MODID = "factoryplanner";
 
+    public static List<Recipe<?>> r = new ArrayList<>();
+
+
     public FactoryPlanner(FMLJavaModLoadingContext context){
 
         IEventBus modEventBus = context.getModEventBus();
         ItemInit.ITEMS.register(modEventBus);
+
+        r.clear();
+
     }
+
+    @Mod.EventBusSubscriber(modid = MODID)
+    public static class ForgeEvents{
+        @SubscribeEvent
+        public static void onRecipeUpdated(RecipesUpdatedEvent event){
+            RecipeManager manager = event.getRecipeManager();
+            manager.getRecipes().forEach((recipe) -> {
+                // client-side usage
+                r.add(recipe);
+            }
+
+            );
+
+            System.out.println("Loaded recipes:" + r.size());
+        }
+    }
+
+
+
 
 }
